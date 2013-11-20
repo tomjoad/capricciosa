bcfaa66204f08e6125c5ee89f46f5c500070426e
lukasz <lukasz@lukasz-ThinkPad-T61.(none)>
2013-11-20 19:11:39 +0100

    second

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Capricciosa::Application.config.secret_key_base = 'd26f90fcfb1af529ec72885a47df07d3f4804956359436b366dfb294597e0f2d125c875e073208879de8f22209cda9f6f686d01e2635fd15a4dab08eff773e55'
